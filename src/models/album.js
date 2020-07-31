3eee86ef9a45baa88eb3cb2a7e5171e316bb7511
Felipe Trost <felipe.trost@gmail.com>
2020-07-31 18:51:24 -0400

    Updated models

--- src/models/album.js
+++ src/models/album.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const albumSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    album_type: {
-        type: String,
-        required: true
-    },
-    images: [{
-        height: Number,
-        width: Number,
-        url: String
-    }],
-    hrefs: {
-        spotify: String,
-        deezer: String,
-    },
-    uids: {
-        spotify: String,
-        deezer: String,
-    },
-    release_date: {
-        type: Date,
-        required: true
-    },
-    total_tracks: {
-        type: Number,
-        required: true
-    },
-    tracks: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Track'
-    }],
-})
+  name: {
+    type: String,
+    required: true
+  },
+  album_type: {
+    type: String,
+    required: true
+  },
+  images: [{
+    height: Number,
+    width: Number,
+    url: String
+  }],
+  hrefs: {
+    spotify: String,
+    deezer: String,
+  },
+  uids: {
+    spotify: String,
+    deezer: String,
+  },
+  release_date: {
+    type: Date,
+    required: true
+  },
+  total_tracks: {
+    type: Number,
+    required: true
+  },
+  tracks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Track'
+  }],
+});
 
-module.exports = mongoose.model("Album", albumSchema);
+module.exports = mongoose.model('Album', albumSchema);
--- src/models/track.js
+++ src/models/track.js
@@ -1,26 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const trackSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    duration_ms: {
-        type: Number,
-        required: true
-    },
-    hrefs: {
-        spotify: String,
-        deezer: String,
-    },
-    previews: {
-        spotify: String,
-        deezer: String,
-    },
-    uids: {
-        spotify: String,
-        deezer: String,
-    },
-})
+  name: {
+    type: String,
+    required: true
+  },
+  duration_ms: {
+    type: Number,
+    required: true
+  },
+  hrefs: {
+    spotify: String,
+    deezer: String,
+  },
+  previews: {
+    spotify: String,
+    deezer: String,
+  },
+  uids: {
+    spotify: String,
+    deezer: String,
+  },
+  album_name: {
+    type: String,
+    required: true
+  },
+  album_mongo_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Album'
+  },
+});
 
-module.exports = mongoose.model("Track", trackSchema);
+module.exports = mongoose.model('Track', trackSchema);
