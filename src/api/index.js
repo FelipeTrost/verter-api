8d9c3bde90494cee12b0b45e6a0e54fa760781bc
Felipe Trost <felipe.trost@gmail.com>
2020-08-01 21:36:05 -0400

    Added more endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+
+const Album = require('../models/album.js');
+const Track = require('../models/track.js');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -6,5 +10,35 @@ router.get('/', (req, res) => {
     message: 'Hello 🌏 - API'
   });
 });
+
+router.get('/albums', async (req, res) => {
+  const albums = await Album.find();
+  res.json({albums});
+});
+
+
+router.get('/album/:name', async (req, res) => {
+  const album = await Album.findOne({name: req.params.name}).populate("tracks");
+  res.json({album});
+});
+
+
+router.get('/tracks', async (req, res) => {
+  const tracks = await Track.find();
+  res.json({tracks});
+});
+
+
+router.get('/track/:name', async (req, res) => {
+  const track = await Track.findOne({name: req.params.name});
+  res.json({track});
+});
+
+
+router.get('/all', async (req, res) => {
+  const albums = await Album.find().populate("tracks");
+  res.json({albums});
+});
+
 
 module.exports = router;
